24357e1aa9dcafb1c842f6def529f98574fd3efb
Renan Lobato Rheinboldt <renan.lobato.rheinboldt@gmail.com>
2021-03-02 17:00:26 -0300

    add pokemons as nested forms

--- app/models/trade.rb
+++ app/models/trade.rb
@@ -1,5 +1,6 @@
 class Trade < ApplicationRecord
   has_many :pokemons, dependent: :destroy, inverse_of: :trade
+  accepts_nested_attributes_for :pokemons, allow_destroy: true
 
   validates :base_experience_difference, presence: true
   validate :must_have_at_least_one_pokemon_on_each_side_of_the_trade, :max_6_pokemons_on_each_side
